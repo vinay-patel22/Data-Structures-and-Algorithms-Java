b8c242b9756ae98642dbd0603cfa14594e83cbad
Vinay Patel <vinaypatel5519@gmai.com>
2023-03-18 12:19:07 +0530

    commit

---
+++ Recursion/Recursion_Striver's/Lec_8_Combination_Sum.java
@@ -0,0 +1,35 @@
+// Leetcode 39
+/* 
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+
+*/
+
+// TC:- 2^t * k
+// SC:- k * x (k=auxilary space and x is number of combination)
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+         List<List<Integer>> ans = new ArrayList<>();
+        findCombi(0,candidates,target,ans,new ArrayList<>());
+        return ans;
+    }
+    public void findCombi(int ind,int[]arr,int target, List<List<Integer>>ans,List<Integer>ds){
+        if(ind==arr.length){
+            if(target==0){
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+        
+        if(arr[ind]<=target){
+            ds.add(arr[ind]);
+            findCombi(ind,arr,target-arr[ind],ans,ds);
+            ds.remove(ds.size()-1);
+        }
+        findCombi(ind+1,arr,target,ans,ds);
+    }
+}
