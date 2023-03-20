6b94685ddc235c69a8e52dd8e93d0faf45d85eef
Vinay Patel <vinaypatel5519@gmai.com>
2023-03-20 17:57:17 +0530

    update

--- Recursion/Recursion_Striver's/Lec_9_Combination_Sum2.java
+++ Recursion/Recursion_Striver's/Lec_9_Combination_Sum2.java
@@ -1,13 +1,7 @@
 // Leetcode 40
 
-/* 
-Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
 
-Each number in candidates may only be used once in the combination.
-
-Note: The solution set must not contain duplicate combinations.
-
-*/
+// Note: The solution set must not contain duplicate combinations.
 /*
 Input: candidates = [10,1,2,7,6,1,5], target = 8
 Output: 
