c5de95409d924491792e8f9af847465c86bb275b
Vinay Patel <121702540+vinay-patel22@users.noreply.github.com>
2023-05-04 14:52:57 +0530

    Update and rename file

--- LeetCode.java
+++ LeetCode.java
@@ -47,3 +47,39 @@ class Solution {
        return ans;
     }
 }
+
+
+
+
+// 649. Dota2 Senate
+
+
+
+// For now i don`t understand what the question is say  . ?:)
+class Solution {
+    public String predictPartyVictory(String senate) {
+        Queue<Integer> qr = new LinkedList<>();
+        Queue<Integer> qd = new LinkedList<>();
+        int n = senate.length();
+
+        for (int i = 0; i < n; i++) {
+            if (senate.charAt(i) == 'R') {
+                qr.offer(i);
+            } else {
+                qd.offer(i);
+            }
+        }
+
+        while (!qr.isEmpty() && !qd.isEmpty()) {
+            int r_id = qr.poll();
+            int d_id = qd.poll();
+            if (r_id < d_id) {
+                qr.offer(r_id + n);
+            } else {
+                qd.offer(d_id + n);
+            }
+        }
+
+        return (qr.size() > qd.size()) ? "Radiant" : "Dire";
+    }
+}
