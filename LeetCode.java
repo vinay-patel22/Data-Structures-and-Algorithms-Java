425a19b20a5005f4fc9c816fbcf47cf68e2b2a6d
Vinay Patel <121702540+vinay-patel22@users.noreply.github.com>
2023-05-07 14:48:47 +0530

    1964. Find the Longest Valid Obstacle Course at Each Position

--- LeetCode.java
+++ LeetCode.java
@@ -194,3 +194,49 @@ class Solution {
         return res;
     }
 }
+
+
+
+// 1964. Find the Longest Valid Obstacle Course at Each Position
+
+
+
+
+
+class Solution {
+    public int[] longestObstacleCourseAtEachPosition(int[] obstacles) {
+        int n = obstacles.length;
+        int[] dp = new int[n];  // stores the smallest ending number of an increasing subsequence
+        int[] ans = new int[n]; // stores the length of the longest increasing subsequence that includes each obstacle
+        
+        int len = 0; // length of the longest increasing subsequence seen so far
+        
+        for (int i = 0; i < n; i++) {
+            int idx = binarySearch(dp, 0, len - 1, obstacles[i]); // find the position where we can add the current obstacle
+            
+            dp[idx] = obstacles[i]; // add the current obstacle to the dp array at the correct position
+            
+            if (idx == len) { // if we added the current obstacle to the end of the dp array
+                len++; // update the length of the longest increasing subsequence seen so far
+            }
+            
+            ans[i] = idx + 1; // update the length of the longest increasing subsequence that includes the i-th obstacle
+        }
+        
+        return ans;
+    }
+    
+    // binary search to find the position where we can add the current obstacle in the dp array
+    private int binarySearch(int[] dp, int start, int end, int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (dp[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+}
+
