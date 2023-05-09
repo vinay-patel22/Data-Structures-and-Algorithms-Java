3c6a517167e97368e9bd58b5c4ec8c8c87a9c23c
Vinay Patel <121702540+vinay-patel22@users.noreply.github.com>
2023-05-09 12:12:18 +0530

    54. Spiral Matrix

--- LeetCode.java
+++ LeetCode.java
@@ -275,3 +275,52 @@ class Solution {
 
 
 
+// 54. Spiral Matrix
+
+
+
+
+class Solution {
+    
+    public List<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer>ans=new ArrayList<Integer>();
+
+        if(matrix.length==0){
+            return ans;
+        }
+        int top=0,left=0;
+        int bottom=matrix.length-1,right=matrix[0].length-1;
+
+        while(top<=bottom && left<=right){
+            // top row
+            for(int i=left;i<=right;i++){
+                ans.add(matrix[top][i]);
+            }
+            top++;
+
+            // right column
+            for(int i=top;i<=bottom;i++){
+                ans.add(matrix[i][right]);
+            }
+            right--;
+
+            // bottom row
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            // left colum
+
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+
+        }
+        return ans;
+    }
+}
