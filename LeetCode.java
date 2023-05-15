c0556a73daf5dcef3452c6e8c08a9661953df0ff
Vinay Patel <121702540+vinay-patel22@users.noreply.github.com>
2023-05-15 13:17:58 +0530

    1721. Swapping Nodes in a Linked List

--- LeetCode.java
+++ LeetCode.java
@@ -548,3 +548,43 @@ public class Solution {
 
 
 }
+
+
+// 1721. Swapping Nodes in a Linked List
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode itr = head;
+        int cnt=1;
+
+    // Traverse the list until reaching the k-th node
+        while(cnt++<k){
+            itr = itr.next;
+        }
+        ListNode first = itr;
+        ListNode second = head;
+
+
+        while(itr.next!=null){
+            itr=itr.next;
+            second = second.next;
+        }
+
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+
+        return head;
+
+    }
+}
