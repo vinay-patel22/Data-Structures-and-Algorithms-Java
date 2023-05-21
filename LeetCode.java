f035ed36663beb715a224a42719e5925a2aaf56b
Vinay Patel <121702540+vinay-patel22@users.noreply.github.com>
2023-05-21 11:49:09 +0530

    934. Shortest Bridge

--- LeetCode.java
+++ LeetCode.java
@@ -822,3 +822,80 @@ class Solution {
         return graph;
     }
 }
+
+
+
+// 934. Shortest Bridge
+
+
+// Time Complexity: O(N^2)
+// Space Complexity: O(N^2)
+
+class Solution {
+    public int shortestBridge(int[][] grid) {
+        Queue<int[]> q = new LinkedList<>();
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, -1, 0, 1};
+        int n = grid.length;
+        boolean[][] vis = new boolean[n][n];
+        boolean breakEnable = false;
+
+        // Step 1: Find the first island using DFS and mark its cells as visited
+        for (int i = 0; i < n; i++) {
+            if (breakEnable)
+                break;
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    formGraph(grid, q, i, j, n, vis, dx, dy);
+                    breakEnable = true;
+                    break;
+                }
+            }
+        }
+
+        int minimumSteps = 0;
+        // Step 2: Perform BFS to find the shortest bridge to the second island
+        while (!q.isEmpty()) {
+            int size = q.size();
+            while (size-- > 0) {
+                int[] curr = q.remove();
+                for (int i = 0; i < 4; i++) {
+                    int newX = curr[0] + dx[i];
+                    int newY = curr[1] + dy[i];
+                    if (isValid(newX, newY, n)) {
+                        if (!vis[newX][newY] && grid[newX][newY] == 1) {
+                            return minimumSteps;
+                        } else if (!vis[newX][newY] && grid[newX][newY] == 0) {
+                            q.add(new int[] { newX, newY });
+                            vis[newX][newY] = true;
+                        }
+                    }
+                }
+            }
+            minimumSteps++;
+        }
+
+        return -1; // No bridge found
+    }
+
+    // Check if the coordinates are within the grid
+    public boolean isValid(int i, int j, int n) {
+        return i >= 0 && j >= 0 && i < n && j < n;
+    }
+
+    // Use DFS to find the first island and mark its cells as visited
+    public void formGraph(int[][] grid, Queue<int[]> q, int x, int y, int n, boolean[][] vis, int[] dx, int[] dy) {
+        if (!isValid(x, y, n) || vis[x][y] || grid[x][y] != 1) {
+            return;
+        }
+
+        q.add(new int[] { x, y });
+        vis[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+            formGraph(grid, q, newX, newY, n, vis, dx, dy);
+        }
+    }
+}
