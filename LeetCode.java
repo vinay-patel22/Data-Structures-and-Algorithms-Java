22b734e4ce597193b09af4d872208ff1b36be2b1
Vinay Patel <121702540+vinay-patel22@users.noreply.github.com>
2023-05-26 13:52:02 +0530

    1140. Stone Game II

--- LeetCode.java
+++ LeetCode.java
@@ -1057,3 +1057,44 @@ class Solution {
         return probability;  // Return the probability for totals >= k and <= n
     }
 }
+
+
+
+// 1140. Stone Game II
+
+
+// It achieves a time complexity of O(n^2) and a space complexity of O(n^2), which is an efficient approach for solving the problem.
+
+class Solution {
+    public int stoneGameII(int[] piles) {
+        int n = piles.length;
+        int[] suffixSum = new int[n];
+        suffixSum[n - 1] = piles[n - 1];
+
+        // Calculate suffix sums for efficient computation
+        for (int i = n - 2; i >= 0; i--) {
+            suffixSum[i] = suffixSum[i + 1] + piles[i];
+        }
+
+        // Initialize the dynamic programming array
+        int[][] dp = new int[n][n + 1];
+
+        // Solve the problem bottom-up using dynamic programming
+        for (int i = n - 1; i >= 0; i--) {
+            for (int M = 1; M <= n; M++) {
+                if (2 * M >= n - i) {
+                    // If 2 * M is greater than or equal to the remaining piles, Alice can take all stones
+                    dp[i][M] = suffixSum[i];
+                } else {
+                    // Calculate the maximum number of stones Alice can obtain by considering different X values
+                    for (int X = 1; X <= 2 * M; X++) {
+                        dp[i][M] = Math.max(dp[i][M], suffixSum[i] - dp[i + X][Math.max(M, X)]);
+                    }
+                }
+            }
+        }
+
+        // The result is the maximum number of stones Alice can obtain starting from the first pile with M = 1
+        return dp[0][1];
+    }
+}
