aff4462b3a6ff0cbe92cbce18e50b400850d75d2
Vinay Patel <121702540+vinay-patel22@users.noreply.github.com>
2023-05-27 10:08:25 +0530

    1406. Stone Game III

--- LeetCode.java
+++ LeetCode.java
@@ -1098,3 +1098,38 @@ class Solution {
         return dp[0][1];
     }
 }
+
+
+
+
+// 1406. Stone Game III
+
+
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+        int n = stoneValue.length;
+        int[] dp = new int[3]; // DP array to store maximum scores
+        
+        for (int i = n - 1; i >= 0; i--) {
+            int takeOne = stoneValue[i] - dp[(i + 1) % 3]; // Score if only one stone is taken
+            
+            int takeTwo = Integer.MIN_VALUE;
+            if (i + 1 < n)
+                takeTwo = stoneValue[i] + stoneValue[i + 1] - dp[(i + 2) % 3]; // Score if two stones are taken
+            
+            int takeThree = Integer.MIN_VALUE;
+            if (i + 2 < n)
+                takeThree = stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp[(i + 3) % 3]; // Score if three stones are taken
+            
+            dp[i % 3] = Math.max(Math.max(takeOne, takeTwo), takeThree); // Store maximum score for the current position
+        }
+        
+        int value = dp[0]; // Alice's score at the start
+        if (value > 0)
+            return "Alice";
+        else if (value < 0)
+            return "Bob";
+        else
+            return "Tie";
+    }
+}
