24af30c1aaa88512c00afb2fbd36ef247eef8b95
Vinay Patel <vinaypatel5519@gmai.com>
2023-06-08 10:28:07 +0530

    Patterns Added

---
+++ Pattern/Patterns.java
@@ -0,0 +1,266 @@
+class Patterns {
+    public static void main(String[] args) {
+        // pattern1(5);;
+        // pattern2(5);;
+        // pattern3(5);
+        // pattern4(5);
+        // pattern5(5);
+        // pattern6(5);
+        // pattern7(5);
+        // pattern8(5);
+        // pattern9(5);
+        // pattern10(5);
+        // pattern11(5);
+        // pattern12(5);
+        pattern13(5);
+
+    }
+
+    public static void pattern1(int n) {
+        // *****
+        // *****
+        // *****
+        // *****
+        // *****
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern2(int n) {
+        // *
+        // **
+        // ***
+        // ****
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern3(int n) {
+        // 1
+        // 12
+        // 123
+        // 1234
+        // 12345
+        // 123456
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern4(int n) {
+        // 1
+        // 22
+        // 333
+        // 4444
+        // 55555
+        // 666666
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern5(int n) {
+        // ******
+        // *****
+        // ****
+        // ***
+        // **
+        // *
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern6(int n) {
+
+        // 12345
+        // 1234
+        // 123
+        // 12
+        // 1
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern7(int n) {
+        // *
+        // ***
+        // *****
+        // *******
+        // *********
+
+        // space , star , space ( This is how we break our code)
+        for (int i = 0; i < n; i++) {
+            // space
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            // star
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+
+            }
+            // space
+            for (int j = 0; j <= n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern8(int n) {
+
+        // *********
+        // *******
+        // *****
+        // ***
+        // *
+
+        // space , star , space ( This is how we break our code)
+        for (int i = 0; i < n; i++) {
+            // space
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            // star
+            for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+                System.out.print("*");
+
+            }
+            // space
+            for (int j = 0; j <= n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Pattern 9
+
+    // *
+    // ***
+    // *****
+    // *******
+    // *********
+    // *********
+    // *******
+    // *****
+    // ***
+    // *
+
+    // Just run pattern(7) and pattern(8) combine in the main code.
+
+    public static void pattern10(int n) {
+
+        // *
+        // **
+        // ***
+        // ****
+        // *****
+        // ****
+        // ***
+        // **
+        // *
+
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            int stars = i;
+            if (i > n)
+                stars = 2 * n - i; // This is logic for print reverse triangle after 5 star printing.
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern11(int n) {
+
+        // 1
+        // 01
+        // 101
+        // 0101
+        // 10101
+
+        int start = 1;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0)
+                start = 1;
+            else
+                start = 0;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(start);
+                start = 1 - start; // This is for fliping number( 1->0 & 0->1 )
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern12(int n) {
+
+        // 1        1
+        // 12      21
+        // 123    321
+        // 1234  4321
+        // 1234554321
+
+        int space = 2 * (n - 1);
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+            space -= 2;
+        }
+    }
+
+    public static void pattern13(int n){
+
+        // 1
+        // 23
+        // 456
+        // 78910
+        // 1112131415
+
+        int num=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num);
+                num = num + 1;
+            }
+            System.out.println();
+        }
+    }
+
+
+}
