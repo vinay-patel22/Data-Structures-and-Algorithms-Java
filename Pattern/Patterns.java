9a95ee8b43acfc069cca9e0d891a1d9b6c4c0be5
Vinay Patel <vinaypatel5519@gmai.com>
2023-06-08 11:43:06 +0530

    Tricks and some other  pattern added

--- Pattern/Patterns.java
+++ Pattern/Patterns.java
@@ -1,3 +1,20 @@
+// Never Asked by product based company like ( MAANG ) .
+// Sometimes Asked by service based company.
+
+// Patterns ----> (Nested Loops)
+
+// Trick to solve any pattern question
+
+// 1) for the outer loop , count the number of lines.
+
+// 2) for the inner loop , focus on the colums & connent them somehow to the rows. 
+
+// 3) print "*" inside the inner for loop.
+
+// 4) Observe symmetry [ Optimal ]
+
+
+
 class Patterns {
     public static void main(String[] args) {
         // pattern1(5);;
@@ -13,7 +30,15 @@ class Patterns {
         // pattern11(5);
         // pattern12(5);
         // pattern13(5);
-        pattern14(5);
+        // pattern14(5);
+        // pattern15(5);
+        // pattern16(5);
+        // pattern17(5);
+        // pattern18(5);
+        // pattern19(5);
+        // pattern20(5);
+        // pattern21(5);
+        // pattern22(4);
 
     }
 
@@ -278,6 +303,214 @@ class Patterns {
             System.out.println();
         }
 
+    }
+    public static void pattern15(int n){
+
+        // ABCDE
+        // ABCD 
+        // ABC  
+        // AB   
+        // A  
+
+        for (int i = 0; i < n; i++) {
+            for (char ch = 'A'; ch <= 'A'+(n-i-1); ch++) {
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+
+    }
+  
+    public static void pattern16(int n){
+
+        // A
+        // BB
+        // CCC
+        // DDDD
+        // EEEEE
+
+        for (int i = 0; i < n; i++) {
+            char ch = (char) ('A' + i);
+            for (int j = 0; j <= i; j++) {
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+
+    }
+    
+    public static void pattern17(int n){
+
+    //      A    
+    //     ABA
+    //    ABCBA
+    //   ABCDCBA
+    //  ABCDEDCBA
+
+        for (int i = 0; i < n; i++) {
+            // space
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            // star
+            char ch = 'A';
+            int breakpoint = (2*i+1)/2;
+            for(int j=1;j<=2*i + 1;j++){
+                System.out.print(ch);
+                // ch++;
+                if(j<=breakpoint)ch++;
+                else ch--;
+            }
+            // space
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+   
+    public static void pattern18(int n){
+
+        // E
+        // DE
+        // CDE
+        // BCDE
+        // ABCDE
+
+        for (int i = 0; i < n; i++) {
+            for(char ch=(char) ('E'-i);ch<='E';ch++){
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+
+    }
+ 
+    public static void pattern19(int n){
+
+        // **********
+        // ****  ****
+        // ***    ***
+        // **      **
+        // *        *
+        // *        *
+        // **      **
+        // ***    ***
+        // ****  ****
+        // **********
+
+        int iniSpace=0;
+       for (int i = 0; i < n; i++) {
+        // star
+        for(int j=1;j<=n-i;j++){
+            System.out.print("*");
+        }
+        // space
+        for(int j=0;j<iniSpace;j++){
+            System.out.print(" ");
+        }
+        // star
+        for(int j=1;j<=n-i;j++){
+            System.out.print("*");
+        }
+        iniSpace += 2;
+        System.out.println();
+       }
+
+
+        iniSpace=2*n -2;
+       for(int i=1;i<=n;i++){
+        // star
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        // space
+        for(int j=0;j<iniSpace;j++){
+            System.out.print(" ");
+        }
+        // star
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        iniSpace -= 2;
+        System.out.println();
+        
+    }
+    }
+
+    public static void pattern22(int n) {
+
+        // 4444444
+        // 4333334
+        // 4322234
+        // 4321234
+        // 4322234
+        // 4333334
+        // 4444444
+
+        for (int i = 0; i < 2*n-1; i++) {
+            for (int j = 0; j < 2*n-1; j++) {
+                int top=i;
+                int left = j;
+                int right = (2*n- 2) -j;
+                int down = (2*n -2) - i;
+                System.out.print(n-Math.min(Math.min(top, down),Math.min(left, right)));
+            }
+            System.out.println();
+        }
+    }
+
+public static void pattern21(int n) {
+
+            // *****
+            // *   *
+            // *   *
+            // *   *
+            // *****
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j == 0|| i== n-1 || j==n-1){
+                    System.out.print("*");
+                }
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+public static void pattern20(int n) {
+
+    // *        *
+    // **      **
+    // ***    ***
+    // ****  ****
+    // **********
+    // ****  ****
+    // ***    ***
+    // **      **
+    // *        *
+
+
+    int spaces = 2*n - 2;
+    for (int i = 1; i <=2*n-1; i++) {
+        int star = i;
+        if(i>n) star = 2*n -i;
+        for (int j = 1; j <= star; j++) {
+            System.out.print("*");
+        }
+        for(int j=1;j<=spaces;j++){
+            System.out.print(" ");
+        }
+        for (int j = 1; j <= star; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        if(i<n) spaces -= 2;
+        else spaces +=2;
+    }
     }
 
 }
